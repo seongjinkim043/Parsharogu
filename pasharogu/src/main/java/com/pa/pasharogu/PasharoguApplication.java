b831ad20b22f4ef90689d17bdc64e0da13999360
seongjinkim043 <seongjinkim043@gmail.com>
2025-06-11 11:28:39 +0900

    First Commit

---
+++ pasharogu/src/main/java/com/pa/pasharogu/PasharoguApplication.java
@@ -0,0 +1,13 @@
+package com.pa.pasharogu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PasharoguApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PasharoguApplication.class, args);
+	}
+
+}
---
+++ pasharogu/src/test/java/com/pa/pasharogu/PasharoguApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pa.pasharogu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PasharoguApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
