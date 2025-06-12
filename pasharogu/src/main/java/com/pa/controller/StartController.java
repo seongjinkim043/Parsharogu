5f639c8f6fcdf4b936e6dbe5f2a1a5fc52bd0830
joy <joy@localhost>
2025-06-12 18:08:30 +0900

    - Start

---
+++ pasharogu/src/main/java/com/pa/controller/StartController.java
@@ -0,0 +1,16 @@
+package com.pa.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class StartController {
+
+
+	
+	@GetMapping("/start")
+	public String start() {
+		return "start";
+	}
+}
